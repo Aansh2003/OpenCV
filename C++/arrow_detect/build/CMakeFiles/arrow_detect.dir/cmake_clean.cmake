538f5421027dbb0204804183eebdc572ddb9557c
Aansh2003 <96300383+Aansh2003@users.noreply.github.com>
2022-11-26 14:49:58 +0530

    arrow_detect

---
+++ C++/arrow_detect/build/CMakeFiles/arrow_detect.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/arrow_detect.dir/arrow_detect_cv.cpp.o"
+  "CMakeFiles/arrow_detect.dir/arrow_detect_cv.cpp.o.d"
+  "arrow_detect"
+  "arrow_detect.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/arrow_detect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
